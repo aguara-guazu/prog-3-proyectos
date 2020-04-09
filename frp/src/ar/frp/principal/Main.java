8aa4866741d8582dad72cca1e33d46af1f0a9087
Luciano Serra <aguara-guazu@users.noreply.github.com>
2020-04-08 22:13:22 -0300

    creacion de multiples archivos para el tp
    creadas 2 pantallas con estilos distintos

---
+++ frp/src/ar/frp/principal/Main.java
@@ -0,0 +1,28 @@
+package ar.frp.principal;
+	
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+
+
+public class Main extends Application {
+	@Override
+	public void start(Stage stage) {
+		try {
+			FXMLLoader loader = new FXMLLoader(getClass().getResource("../view/MainApp.fxml"));
+
+			Scene scene = new Scene(loader.load());
+			stage.setTitle("Servicios a terceros UTN - frp");
+			stage.setScene(scene);
+			stage.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
